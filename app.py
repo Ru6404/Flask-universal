ff7e1124b88a282c3e7cce48729d6236495bfbcb
Ru6404 <102176417+Ru6404@users.noreply.github.com>
2025-05-18 09:34:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route("/")
+def home():
+    return f"API_KEY: {app.config['API_KEY']}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
